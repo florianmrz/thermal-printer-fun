fbc59564f6551f850dac6991e34112821e24e2b6
florianmrz <florian.merz@mail.de>
2026-02-27 23:02:52 +0100

    fix: fix print commands using decimal instead of ascii numbers

--- api/src/index.ts
+++ api/src/index.ts
@@ -10,12 +10,30 @@ const { injectWebSocket, upgradeWebSocket, wss } = createNodeWebSocket({ app });
 app.post("/api/print", (c) => {
   console.log(`Connected clients: ${wss.clients.size}`);
 
-  print();
+  // TODO replace with actual print data
+  const printData = new Array(100)
+    .fill(null)
+    .map(() =>
+      new Array(72).fill(null).map(() => (Math.random() > 0.5 ? 0xff : 0x00)),
+    );
+
+  print(printData, {
+    cutPaper: true,
+    printQuality: "highPrint",
+    lineFeedDots: 200,
+  });
 
   return c.json({ success: true });
 });
 
-function print() {
+function print(
+  printLines: number[][],
+  options?: {
+    cutPaper?: boolean;
+    printQuality?: "highSpeed" | "normal" | "highPrint";
+    lineFeedDots?: number;
+  },
+) {
   const printerClient = Array.from(wss.clients).find(
     (client) => client.readyState === WebSocket.OPEN,
   );
@@ -31,39 +49,65 @@ function print() {
   // ESC * r P n NUL (n = page length, 0 for continous print)
   printerClient.send(Uint8Array.from([0x1b, 0x2a, 0x72, 0x50, 0x00, 0x00]));
 
-  // TODO this seems to not actually make that big of a difference
   // Set raster print quality
   // ESC * r Q n NUL (n = print quality: 0 = high speed, 1 = normal 2 = high print)
-  printerClient.send(Uint8Array.from([0x1b, 0x2a, 0x72, 0x51, 0x00, 0x00]));
+  const printQuality = options?.printQuality ?? "highPrint";
+  const printQualityMap = {
+    highSpeed: 0x30, // 0
+    normal: 0x31, // 1
+    highPrint: 0x32, // 2
+  };
+  printerClient.send(
+    Uint8Array.from([
+      0x1b,
+      0x2a,
+      0x72,
+      0x51,
+      printQualityMap[printQuality],
+      0x00,
+    ]),
+  );
 
   // Set raster FF mode
-  // ESC * r F n NUL (n = mode: 1 = prevents paper cut, 0 = allows paper cut)
-  // TODO doesn't work?
-  // const cutPaper = false;
-  // printerClient.send(Uint8Array.from([0x1b, 0x2a, 0x72, 0x46, cutPaper ? 0x00 : 0x01, 0x00]));
+  // ESC * r F n NUL (n = mode: 0 = allows paper cut, 1 = prevents paper cut)
+  const cutPaper = options?.cutPaper ?? true;
+  printerClient.send(
+    Uint8Array.from([0x1b, 0x2a, 0x72, 0x46, cutPaper ? 0x30 : 0x31, 0x00]),
+  );
 
   // Send raster data (auto line feed)
   // b H 00 + 72 bytes of data
-  for (let i = 0; i < 1; i++) {
-    const data = Uint8Array.from([
-      0x62,
-      0x48,
-      0x00,
-      ...new Array(72)
-        .fill(null)
-        .map(() => (Math.random() > 0.5 ? 0xff : 0x00)),
-    ]);
-    const finalData = [];
-    // Use chunks of 100 lines to avoid overwhelming the printer
-    for (let j = 0; j < 100; j++) {
-      finalData.push(...data);
+  let linesInChunk = 0;
+  const lineChunks: Uint8Array[] = [];
+  let currentChunk: Uint8Array = new Uint8Array();
+  printLines.forEach((line) => {
+    const lineHeader = Uint8Array.from([0x62, 0x48, 0x00]);
+    const lineData = Uint8Array.from(line);
+    const linePacket = new Uint8Array([...lineHeader, ...lineData]);
+    currentChunk = new Uint8Array([...currentChunk, ...linePacket]);
+    linesInChunk++;
+    if (linesInChunk >= 100) {
+      lineChunks.push(currentChunk);
+      currentChunk = new Uint8Array();
+      linesInChunk = 0;
     }
-    printerClient.send(finalData);
-  }
+  });
+  lineChunks.forEach((chunk) => printerClient.send(chunk));
 
   // Move vertical direction position by 100 dots
-  // TODO doesn't work?
-  // printerClient.send(Uint8Array.from([0x1b, 0x2a, 0x72, 0x59, 0x64, 0x00]));
+  // ESC * r Y n NUL (n = number of dots to move)
+  if (
+    options?.lineFeedDots &&
+    typeof options.lineFeedDots === "number" &&
+    options.lineFeedDots > 0
+  ) {
+    const lineFeedAsHex = Array.from(options.lineFeedDots.toString()).map((c) =>
+      c.charCodeAt(0),
+    );
+    printerClient.send(
+      Uint8Array.from([0x1b, 0x2a, 0x72, 0x59, ...lineFeedAsHex, 0x00]),
+    );
+  }
 
   // Execute FF mode (cuts paper)
   // ESC FF NUL
