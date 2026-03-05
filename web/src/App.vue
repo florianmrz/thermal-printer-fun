f2ef424f1bda22b30501685ffedb16f11af50292
florianmrz <florian.merz@mail.de>
2026-03-05 23:01:48 +0100

    feat: add basic web app

---
+++ web/src/App.vue
@@ -0,0 +1,23 @@
+<script setup lang="ts">
+import { RouterLink, RouterView } from 'vue-router';
+import HelloWorld from './components/HelloWorld.vue';
+</script>
+
+<template>
+  <header>
+    <img alt="Vue logo" src="./assets/logo.svg" width="125" height="125" />
+
+    <div>
+      <HelloWorld />
+
+      <nav>
+        <RouterLink to="/">Home</RouterLink>
+        <RouterLink to="/about">About</RouterLink>
+      </nav>
+    </div>
+  </header>
+
+  <RouterView />
+</template>
+
+<style scoped></style>
