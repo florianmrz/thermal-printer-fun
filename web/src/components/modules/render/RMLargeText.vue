c8192b30990dffc397e581652d2f075181833dac
florianmrz <florian.merz@mail.de>
2026-03-21 21:17:17 +0100

    fix: simplify large text logic

--- web/src/components/modules/render/RMLargeText.vue
+++ web/src/components/modules/render/RMLargeText.vue
@@ -1,41 +1,17 @@
 <template>
   <div class="rm-large-text">
-    <div class="canvas">
-      <p ref="textRef" class="text">{{ props.data.input }}</p>
-    </div>
+    {{ props.data.input }}
   </div>
 </template>
 
 <script setup lang="ts">
 import type { RenderDataLargeText } from '@thermal-printer-fun/shared';
-import { nextTick, onMounted, ref } from 'vue';
+import { onMounted } from 'vue';
 import type { RenderModuleProps } from './types';
 
-const CANVAS_SIZE = 576;
-const MIN_FONT_SIZE = 12;
-
 const props = defineProps<RenderModuleProps<RenderDataLargeText>>();
-const textRef = ref<HTMLElement | null>(null);
-
-function fitTextToCanvas() {
-  const textElement = textRef.value;
-
-  if (!textElement) {
-    return;
-  }
-
-  let fontSize = CANVAS_SIZE;
-  textElement.style.fontSize = `${fontSize}px`;
-
-  while (fontSize > MIN_FONT_SIZE && textElement.scrollWidth > CANVAS_SIZE) {
-    fontSize -= 2;
-    textElement.style.fontSize = `${fontSize}px`;
-  }
-}
 
 onMounted(async () => {
-  await nextTick();
-  fitTextToCanvas();
   props.onReady();
 });
 </script>
