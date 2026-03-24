f3aa3b5c6630e60fb55b022ea8f8ca476892e8d6
florianmrz <florian.merz@mail.de>
2026-03-24 21:13:55 +0100

    fix: fix base icon blurry

--- web/src/components/base/BaseIcon/BaseIcon.vue
+++ web/src/components/base/BaseIcon/BaseIcon.vue
@@ -1,25 +1,20 @@
 <template>
-  <component
-    :is="component"
-    class="base-icon"
-    :class="`is-${props.size}`"
-    :viewBox="props.viewBox"
-  />
+  <component :is="component" class="base-icon" :class="`is-${props.size}`" viewBox="0 0 24 24" />
 </template>
 
 <script setup lang="ts">
 import { computed } from 'vue';
 import { icons, type IconName } from './icons';
 
-// TODO icons look blurry, fix
-const props = withDefaults(defineProps<{
-  icon: IconName;
-  size?: 'small' | 'medium' | 'large';
-  viewBox?: string;
-}>(), {
-  size: 'medium',
-  viewBox: '0 0 24 24'
-});
+const props = withDefaults(
+  defineProps<{
+    icon: IconName;
+    size?: 'regular' | 'large';
+  }>(),
+  {
+    size: 'regular',
+  }
+);
 
 const component = computed(() => icons[props.icon]);
 </script>
